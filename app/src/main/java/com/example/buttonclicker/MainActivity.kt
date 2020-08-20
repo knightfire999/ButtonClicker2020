e3c2d54036fbe979bce196b9ef48415bb33d7f58
chris <liu.christopher05@gmail.com>
2020-08-20 11:11:01 -0700

    Button counts the number of times it is clicked and displays it

--- app/src/main/java/com/example/buttonclicker/MainActivity.kt
+++ app/src/main/java/com/example/buttonclicker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.buttonclicker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 private var timesClicked = 0
@@ -16,5 +17,17 @@ class MainActivity : AppCompatActivity() {
     {
         timesClicked++
         button.setText(timesClicked.toString())
+        if(timesClicked == 10)
+        {
+            Toast.makeText(this, "Way to go! 10 is a big number!", Toast.LENGTH_LONG).show()
+        }
+        else if(timesClicked == 50)
+        {
+            Toast.makeText(this, "Wow! Nice button clicking!", Toast.LENGTH_LONG).show()
+        }
+        else if(timesClicked == 100)
+        {
+            Toast.makeText(this, "You're still here? Go study.", Toast.LENGTH_LONG).show()
+        }
     }
 }
